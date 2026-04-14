c96c387255eefd0251a3fde39fdf53452aacbb89
iamderrick184 <kakooza184@gmail.com>
2026-04-14 13:03:58 +0300

    Add Person model admin configuration

--- Derro/school/student/admin.py
+++ Derro/school/student/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from student.models import Person
 # Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display=['name','age','email']
+admin.site.register(Person)
